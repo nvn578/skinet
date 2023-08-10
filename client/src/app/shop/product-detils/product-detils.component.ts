680e7bcfd656cfa5ae2874476d674ede61039805
Naveen Bhatt <naveenbhatt1997@gmail.com>
2023-08-10 22:56:43 +0530

    Adding styling , bootswatch theme and homepage

--- client/src/app/shop/product-detils/product-detils.component.ts
+++ client/src/app/shop/product-detils/product-detils.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-detils',
@@ -12,8 +13,9 @@ export class ProductDetilsComponent implements OnInit {
 
   product?:Product;
 
-  constructor(private shopService: ShopService, private activatedRoute : ActivatedRoute){
+  constructor(private shopService: ShopService, private activatedRoute : ActivatedRoute,private bcService : BreadcrumbService){
 
+    this.bcService.set('@productDetails',' ');
   }
   ngOnInit(): void {
     this.loadProduct();
@@ -22,7 +24,11 @@ export class ProductDetilsComponent implements OnInit {
   loadProduct(){
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if(id) this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => 
+      {
+        this.product = product;
+        this.bcService.set('@productDetails',product.name)
+      },
       error: error => console.log(error)
     }
 
